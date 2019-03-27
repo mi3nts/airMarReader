353216fabd6d643f36f5ada55924ad4163f6635f
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2019-03-27 11:43:19 -0500

    Update mintsSensorReader.py

--- c1Plus/mintsC1Plus/mintsSensorReader.py
+++ c1Plus/mintsC1Plus/mintsSensorReader.py
@@ -121,7 +121,7 @@ def GPVTGWrite(sensorData,dateTime):
     dataLength = 10
     gpsQuality = dataOut[6]
     print(sensorName+"-"+str(dataLength)+"-"+str(len(dataOut)))
-    if(len(dataOut) ==(dataLength +1) and bool(dataOut[1])) ):
+    if(len(dataOut) ==(dataLength +1) and bool(dataOut[1])):
         sensorDictionary = OrderedDict([
                 ("dateTime"               ,str(dateTime)),
         	("courseOGTrue"           ,dataOut[1]),
